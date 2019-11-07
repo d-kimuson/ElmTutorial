bc2ffc15a4264091fc59b1f771396e8b5d90af2d
Kaito <mail.kaito03@gmail.com>
2019-11-07 16:33:31 +0900

    move build dir

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for file in `find src -type f -name *.elm`; do
-  elm make $file --output `echo $file | sed s/src/build/g | sed s/.elm/.js/g`
+  elm make $file --output `echo $file | sed s/src/doc/g | sed s/.elm/.js/g`
 done
